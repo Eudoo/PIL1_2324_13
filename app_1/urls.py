8abd672d29c68c8abb366a9aa3ed83e7dd3f73de
Eudes CODO <eudescodo00@gmail.com>
2024-06-20 23:44:15 +0100

    produit fini du projet

--- app_1/urls.py
+++ app_1/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from .views import vue_connexion, vue_deconnexion,vue_profile, accueil, vue_inscription1, vue_inscription2
 from .views import vue_recommandations, vue_base, vue_recherche, vue_profile_partenaire, vue_like_profile, vue_modifier_profile
+from django.contrib.auth import views as auth_views
 
 
 
 urlpatterns = [
     path('', accueil, name='accueil'),
     path('connexion/', vue_connexion, name='vue_connexion'),
+    path('mot_oublie', auth_views.PasswordResetView.as_view(template_name='mot_oublie.html'), name='vue_mot_oublie'),
+    
     path('deconnexion/', vue_deconnexion, name='vue_deconnexion'),
     path('profile/', vue_profile, name='vue_profile'),
     path('inscription1/', vue_inscription1, name='vue_inscription1'),
